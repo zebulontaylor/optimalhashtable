bb6d9aa771b5245eb411fe6656fd37265cdf663f
Zeb Taylor <zceboys@gmail.com>
2026-02-18 17:35:14 -0600

    insertion fixed; search added

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from math import log2, ceil, floor
 from random import randint
 
-delta = 0.4
-n = 2**6
+delta = 0.1
+n = 2**8
 
 tables = []
 
@@ -41,69 +41,101 @@ assert sum(t.size for t in tables) == n
 
 phi = lambda i, j: int("".join("1" + b for b in bin(j)[2:]) + "0" + bin(i)[2:], 2)
 
-c = 100
-f = lambda epsilon: ceil(c * min(log2(1/epsilon)**2, log2(1/delta)))
+c = 10
+f = lambda epsilon: ceil(c * min(log2(1/epsilon)**2, log2(1/delta))) + 1
 
 p = 2**31-1
 a = randint(1, p-1)
-b = randint(0, p-1)
 
-hphi = lambda i, j: ((a*phi(i, j) + b) % p) % tables[i].size
+hphi = lambda i, j, value: ((a*phi(i, j) + value) % p) % tables[i].size
 
+## INSERTING
 def get_batch_size(batch_no):
-    a1 = tables[batch_no].size
     if batch_no == 0:
-        return ceil(0.75 * a1)
-    a2 = tables[batch_no+1].size
+        return ceil(0.75 * tables[0].size)
+    a1 = tables[batch_no-1].size
+    a2 = tables[batch_no].size
     return a1 - floor(delta * a1 / 2) - ceil(0.75 * a1) + ceil(0.75 * a2)
 
 def insert_from_batch(batch_no, value):
-    table1 = tables[batch_no]
-    table2 = tables[batch_no+1]
+    table1 = tables[batch_no-1]
+    table2 = tables[batch_no]
     
     e1 = table1.epsilon
     e2 = table2.epsilon
 
+    if batch_no == 0:
+        # Do table 1
+        j = 0
+        while True:
+            if table2.probe_empty(hphi(batch_no, j, value)):
+                table2.insert(hphi(batch_no, j, value), value)
+                return (batch_no, hphi(batch_no, j, value), j)
+            j += 1
+
     if e1 > delta / 2 and e2 > 0.25:
         # Try table 1 first up to f(e1), then try table 2
         for j in range(f(e1)):
-            if table1.probe_empty(hphi(batch_no, j)):
-                table1.insert(hphi(batch_no, j), value)
-                return
+            if table1.probe_empty(hphi(batch_no-1, j, value)):
+                table1.insert(hphi(batch_no-1, j, value), value)
+                return (batch_no-1, hphi(batch_no-1, j, value), j)
         j = 0
+        print(f"failed to insert to table 1 using {f(e1)} probes")
         while True:
-            if table2.probe_empty(hphi(batch_no+1, j)):
-                table2.insert(hphi(batch_no+1, j), value)
-                return
+            if table2.probe_empty(hphi(batch_no, j, value)):
+                table2.insert(hphi(batch_no, j, value), value)
+                return (batch_no, hphi(batch_no, j, value), j+f(e1))
             j += 1
     elif e1 <= delta / 2:
         # Do table 2
         j = 0
         while True:
-            if table2.probe_empty(hphi(batch_no+1, j)):
-                table2.insert(hphi(batch_no+1, j), value)
-                return
+            if table2.probe_empty(hphi(batch_no, j, value)):
+                table2.insert(hphi(batch_no, j, value), value)
+                return (batch_no, hphi(batch_no, j, value), j)
             j += 1
     else: # e2 <= 0.25
         # Do table 1
         j = 0
         while True:
-            if table1.probe_empty(hphi(batch_no, j)):
-                table1.insert(hphi(batch_no, j), value)
-                return
+            if table1.probe_empty(hphi(batch_no-1, j, value)):
+                table1.insert(hphi(batch_no-1, j, value), value)
+                return (batch_no-1, hphi(batch_no-1, j, value), j)
             j += 1
+
+## SEARCHING
+valid_probes = []
+for i_idx in range(len(tables)):
+    for j_idx in range(1, 100):
+        cost = phi(i_idx, j_idx)
+        valid_probes.append((cost, i_idx, j_idx))
+
+valid_probes.sort(key=lambda x: x[0])
+
+def search(value):
+    for idx, (_, i, j) in enumerate(valid_probes):
+        slot = hphi(i, j, value)
+        if tables[i].table[slot] == value:
+            return (value, i, j, slot, idx)
+    return None
 
 batch_no = 0
 left_in_batch = get_batch_size(batch_no)
 
-print(f"inserting {int(n * (1-delta))} elements")
-
 for i in range(int(n * (1-delta))):
-    print(f"Inserting {i}; batch_no={batch_no}; left_in_batch={left_in_batch}")
-    insert_from_batch(batch_no, i)
+    table_no, slot, probes = insert_from_batch(batch_no, i)
+    print(f"Inserted {i} in {probes} probes at table {table_no}, slot {slot} (batch {batch_no})")
     left_in_batch -= 1
     if left_in_batch == 0:
+        if batch_no > 0:
+            print(tables[batch_no-1].epsilon)
         batch_no += 1
         left_in_batch = get_batch_size(batch_no)
+
+for i in range(5):
+    num = randint(0, int(n * (1-delta))-1)
+    print(num)
+    val, i, j, slot, idx = search(num)
+    print(f"Found {val} at table {i}, slot {slot}, in {idx+1} probes")
 
 print(tables)

492a7503984daf0197d66189a0e7859c13c23e60
Zeb Taylor <zceboys@gmail.com>
2026-02-18 16:16:27 -0600

    batch stuff and phi

--- test.py
+++ test.py
@@ -36,3 +36,33 @@ for i in range(1, floor(log2(n)) + 1):
     tables.append(SubArray(length))
 
 print(tables)
+assert sum(t.size for t in tables) == n
+
+batch_no = 0
+
+phi = lambda i, j: int("".join("1" + b for b in bin(j)[2:]) + "0" + bin(i)[2:], 2)
+
+def get_batch_size(batch_no):
+    a1 = tables[batch_no].size
+    a2 = tables[batch_no+1].size
+    return a1 - floor(delta * a1 / 2) - ceil(0.75 * a1) + ceil(0.75 * a2)
+
+def insert_from_batch(batch_no):
+    table1 = tables[batch_no]
+    table2 = tables[batch_no+1]
+    
+    e1 = table1.epsilon
+    e2 = table2.epsilon
+
+    if e1 > delta / 2 and e2 > 0.25:
+        for i in range(10): # CHANGE TO F(epsilon) FUNCTION
+            ...
+    elif e1 <= delta / 2:
+        while True:
+            ...
+    else: # e2 <= 0.25
+        while True:
+            ...
+
+for i in range(int(n * (1-delta))):
+    ...

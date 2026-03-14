c9a5ef8f9012af4b2314672a8e4e0f6d9ff3d375
Zeb Taylor <zceboys@gmail.com>
2026-03-14 17:54:48 -0500

    data collection framework

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import elastic
 import naive
-from random import sample, randint
 
-def run_test(insert, search, n, delta, values, num_searches=10000):
+from random import sample, randint
+import csv
+import tqdm
+
+def run_test(insert, search, n, values, num_searches=40000):
     average_insertion_probes = 0
     insertion_count = 0
 
-    for i in range(int(n * (1-delta))):
+    for i in range(len(values)):
         insertion_count += 1
         res = insert(values[i])
         probes = res[2] if isinstance(res, tuple) else res
@@ -24,18 +27,30 @@ def run_test(insert, search, n, delta, values, num_searches=10000):
         average_search_probes += idx
         search_count += 1
 
-    print(f"Average insertion probes: {average_insertion_probes / insertion_count:.3f}")
-    print(f"Average search probes: {average_search_probes / search_count:.3f}")
+    #print(f"Average insertion probes: {average_insertion_probes / insertion_count:.3f}")
+    #print(f"Average search probes: {average_search_probes / search_count:.3f}")
+    return (average_insertion_probes / insertion_count, average_search_probes / search_count)
 
 
 if __name__ == "__main__":
-    delta = 0.05
     n = 2**13
-    values = sample(range(n * 10), int(n * (1 - delta)))
-    eht = elastic.ElasticHashtable(n, delta)
-    nht = naive.NaiveHashtable(n, delta)
-    print(f"Using delta={delta}, n={n}")
-    print("=== Elastic Hashtable ===")
-    run_test(eht.insert, eht.search, n, delta, values)
-    print("=== Naive Hashtable ===")
-    run_test(nht.insert, nht.search, n, delta, values)
+    rows = []
+    for k in tqdm.tqdm(range(2, 23), desc="Deltas", position=0):
+        averages = [0, 0, 0, 0]
+        for i in tqdm.tqdm(range(5), desc="Inner Samples", position=1, leave=False):
+            delta = 1.3**(-k)
+            values = sample(range(n * 100), int(n * (1-delta)))
+            eht = elastic.ElasticHashtable(n, delta)
+            nht = naive.NaiveHashtable(n, delta)
+            e_insert, e_search = run_test(eht.insert, eht.search, n, values)
+            n_insert, n_search = run_test(nht.insert, nht.search, n, values)
+            averages[0] += e_insert
+            averages[1] += e_search
+            averages[2] += n_insert
+            averages[3] += n_search
+        rows.append([delta, averages[0]/5, averages[1]/5, averages[2]/5, averages[3]/5])
+    
+    with open("results.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["delta", "elastic_insert", "elastic_search", "naive_insert", "naive_search"])
+        writer.writerows(rows)
